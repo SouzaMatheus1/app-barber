55cd3350e5fa290ea62bcb9abe3a9ff62586f33d
SouzaMatheus <matheus3864@gmail.com>
2026-06-02 14:06:38 -0300

    feat: implementa a parte de PWA ja com config de manifesto para web

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,55 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Lambda M - Portal do Cliente',
+        short_name: 'Lambda M',
+        description: 'Portal de Agendamento do Cliente Lambda M',
+        theme_color: '#09090b', // Cor escura correspondente ao Zinc 950 do Tailwind v4
+        background_color: '#09090b', // Cor escura correspondente ao Zinc 950 do Tailwind v4
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true // Permite testar o PWA no ambiente de desenvolvimento
+      }
+    })
   ],
   test: {
     globals: true,
@@ -14,3 +57,4 @@ export default defineConfig({
     setupFiles: './vitest.setup.ts',
   },
 })
+
